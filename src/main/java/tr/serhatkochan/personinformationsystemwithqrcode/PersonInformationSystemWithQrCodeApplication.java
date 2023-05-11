2fccecb936ec4adeba69e1edf13dc116f5bf2207
serhatkochan <serhatkochan@hotmail.com.tr>
2023-05-11 20:04:42 +0300

    swagger3

--- src/main/java/tr/serhatkochan/personinformationsystemwithqrcode/PersonInformationSystemWithQrCodeApplication.java
+++ src/main/java/tr/serhatkochan/personinformationsystemwithqrcode/PersonInformationSystemWithQrCodeApplication.java
@@ -1,9 +1,15 @@
 package tr.serhatkochan.personinformationsystemwithqrcode;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
+@SecurityScheme(name = "Authorization", type = SecuritySchemeType.APIKEY, in = SecuritySchemeIn.HEADER)
 public class PersonInformationSystemWithQrCodeApplication {
 
     public static void main(String[] args) {
